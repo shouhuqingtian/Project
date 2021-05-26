deae23fbb91acb2e2e508f17894b4536e4da959a
shouhuqingtian <519328539@qq.com>
2021-05-26 14:03:58 +0800

    添加python命令行适配

--- main/run_test.py
+++ main/run_test.py
@@ -1,5 +1,9 @@
 # -*- coding:utf-8 -*-
 import sys
+import os
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
 env_variable = [r'D:\Python Project\main',
                 r'D:\Python Project',
                 r'C:\Program Files\JetBrains\PyCharm 2020.1.2\plugins\python\helpers\pycharm_display',
